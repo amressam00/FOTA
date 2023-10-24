26e93e0573290613f2fa98949d51292a0a96815f
amressam00 <47486312+amressam00@users.noreply.github.com>
2023-10-24 20:46:02 +0300

    Delete stm32code directory

--- stm32code/Debug/src/Parse.d
+++
@@ -1,8 +0,0 @@
-src/Parse.o: ../src/Parse.c ../include/STD_TYPES.h ../include/BIT_MATH.h \
- ../include/FPEC_interface.h
-
-../include/STD_TYPES.h:
-
-../include/BIT_MATH.h:
-
-../include/FPEC_interface.h:
